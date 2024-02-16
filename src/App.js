a693975e56b5d570f851407cc9c45f82fff8eca1
Siddhant Jain <siddhant.jain0607@gmail.com>
2024-02-16 22:07:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Confetti from "react-confetti";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [size, setSize] = useState(1);
+
+	const handleClick = () => {
+		setSize(size + 0.2);
+	};
+
+	const [confetti, setConfetti] = useState(false);
+
+	const throwConfetti = () => {
+		setConfetti(true);
+	};
+
+	return (
+		<div className="App">
+			{confetti ? (
+				<div>
+					<h1>Thank you so much! I promise you won't regret this!</h1>
+					{confetti && <Confetti />}
+				</div>
+			) : (
+				<div>
+					<h2>I know I messed up BIG TIME</h2>
+					<h2>I'm stupid</h2>
+					<h1>I'm so sorry</h1>
+					<h1>
+						Will you please forgive me and do me the honour of going out with
+						me?
+					</h1>
+					<div className="buttons">
+						<button
+							className="yes-btn"
+							style={{
+								transform: `scale(${size})`,
+								transition: "transform 0.3s ease",
+							}}
+							onClick={throwConfetti}
+						>
+							Yes
+						</button>
+
+						<button className="no-btn" onClick={handleClick}>
+							No
+						</button>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
